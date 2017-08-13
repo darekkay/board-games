33c685b1addd22de5ef92e01a4629596c2c6a1c3
Darek Kay <darekkay@gmail.com>
2017-08-13 11:24:37 +0200

    Fix default player count

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {player: 3};
+    this.state = {player: 1};
   }
 
   renderGames() {
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -19,7 +19,7 @@ class Header extends Component {
           />
           <div className="logo">
             <img src="img/cube.png" alt=""/>
-            <div class="title">Board Games</div>
+            <div className="title">Board Games</div>
           </div>
         </header>
     );
