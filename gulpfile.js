9c2f554ea5b5264ebd4b2cffc81a6f4cc1b807cf
Darek Kay <darekkay@gmail.com>
2017-08-28 10:31:53 +0200

    Move games data to /data, replace yaml conversion with gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const gulp = require('gulp');
+const yaml = require('gulp-yaml');
+
+const folder = {
+  yamlSrc: 'data/*.yml',
+  yamlDest: 'src/data/'
+};
+
+gulp.task('yaml', ['yaml-convert', 'watch']);
+
+gulp.task('yaml-convert', () => {
+  /* Convert YAML → JSON */
+  return gulp.src(folder.yamlSrc)
+      .pipe(yaml())
+      .pipe(gulp.dest(folder.yamlDest));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(folder.yamlSrc, ['yaml-convert']);
+});
