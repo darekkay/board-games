04f524ce55476b0a24b414878911c2c1c9752392
Darek Kay <darekkay@gmail.com>
2018-10-10 15:50:50 +0200

    Sync Prettier settings with IDEA formatter

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const yaml = require('gulp-yaml');
+const gulp = require("gulp");
+const yaml = require("gulp-yaml");
 
 const folder = {
-  yamlSrc: 'data/*.yml',
-  yamlDest: 'src/data/'
+  yamlSrc: "data/*.yml",
+  yamlDest: "src/data/"
 };
 
-gulp.task('yaml', ['yaml-convert', 'watch']);
+gulp.task("yaml", ["yaml-convert", "watch"]);
 
-gulp.task('yaml-convert', () => {
+gulp.task("yaml-convert", () => {
   /* Convert YAML → JSON */
-  return gulp.src(folder.yamlSrc)
-      .pipe(yaml())
-      .pipe(gulp.dest(folder.yamlDest));
+  return gulp
+    .src(folder.yamlSrc)
+    .pipe(yaml())
+    .pipe(gulp.dest(folder.yamlDest));
 });
 
-gulp.task('watch', () => {
-  gulp.watch(folder.yamlSrc, ['yaml-convert']);
+gulp.task("watch", () => {
+  gulp.watch(folder.yamlSrc, ["yaml-convert"]);
 });
