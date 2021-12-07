cef596c9e693aeb35c75292b40a785038805d11c
Darek Kay <hello@darekkay.com>
2021-12-07 14:10:37 +0100

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +18,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [reactRefresh()],
+  plugins: [react()],
 });
